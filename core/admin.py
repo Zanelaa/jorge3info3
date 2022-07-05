e8a175ac16a6b4a6c17b0676416c3e837f453a48
jorge <jorge.zanela19@gmail.com>
2022-07-05 09:43:35 -0300

    inicial

---
+++ core/admin.py
@@ -0,0 +1,8 @@
+from re import A
+from django.contrib import admin
+
+from core.models import Categoria,Editora,Autor
+
+admin.site.register(Categoria)
+admin.site.register(Editora)
+admin.site.register(Autor)
