93caf0f66562c37dce12ea67036198d3203d525d
Zanelaa <jorge.zanela19@gmail.com>
2022-07-07 16:51:44 -0300

    add_livros

--- core/models.py
+++ core/models.py
@@ -1,5 +1,3 @@
-import email
-from unittest.util import _MAX_LENGTH
 from django.db import models
 
 class Categoria(models.Model):
@@ -23,3 +21,11 @@ class Autor(models.Model):
 
     class Meta:
         verbose_name_plural = "Autores"
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32)
+    quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros")
+    Editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
