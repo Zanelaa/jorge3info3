17dd25ee2cb988163429ed19df4bc5f0b49f6371
Zanelaa <jorge.zanela19@gmail.com>
2022-08-22 14:54:21 -0300

    Add-views,urls e testanto

--- livraria/urls.py
+++ livraria/urls.py
@@ -1,15 +1,17 @@
-
 from django.contrib import admin
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from core.views import CategoriaViewSet
+from core.views import CategoriaViewSet, AutorViewSet, EditoraViewSet, LivroViewSet
 
 router = DefaultRouter()
-router.register(r'categoria',CategoriaViewSet)
+router.register(r"Categorias", CategoriaViewSet)
+router.register(r"Autores", AutorViewSet)
+router.register(r"Editoras", EditoraViewSet)
+router.register(r"Livros", LivroViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include(router.urls)),
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
 ]
